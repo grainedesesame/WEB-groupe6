da482148d19760d12545cc20e9056c92a6ee736d
grainedesesame <celine.wang@centrale.centralelille.fr>
2021-04-12 20:34:37 +0200

    nouveau fichier js

---
+++ js/lib_templates.js
@@ -0,0 +1,8 @@
+/* librairie destinées aux pages de template */
+/* Redirections */
+function rdFormulaire(){
+    $(location).attr('href', "../formulaire/formulaire.html");
+}
+function rdAccueil(){
+    $(location).attr('href', "../accueil/accueil.html");
+}
